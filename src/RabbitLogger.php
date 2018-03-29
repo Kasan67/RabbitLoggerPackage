0239b9eee3cb0ed9b938e54012efcc8d9c16b234
Kasan <aleksandr.kashirin@a-bank.com.ua>
2018-03-29 16:44:34 +0300

    initial

---
+++ src/RabbitLogger.php
@@ -0,0 +1,15 @@
+<?php
+namespace kashirin\rabbit_mq;
+/**
+ * User: kasan
+ * Date: 3/29/18
+ * Time: 4:34 PM
+ */
+
+use PhpAmqpLib\Wire\IO\StreamIO;
+use PhpAmqpLib\Connection\AbstractConnection;
+
+class RabbitLogger
+{
+
+}
